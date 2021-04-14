321f16fc64e1ddf59f25c1dc431206a7b53d6965
Ageesa <ageesapareek321@gmail.com>
2021-04-14 14:51:17 -0400

    Landing page complete and bottom nav functional

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
-$("body").css( "background color", "black")
+$(window).on("load", start);
+
+function start(){
+  $("#btmnav a:first").click()
+}
 function openNav() {
     document.getElementById("mySidepanel").style.width = "250px";
   }
@@ -6,4 +10,19 @@ function openNav() {
   /* Set the width of the sidebar to 0 (hide it) */
   function closeNav() {
     document.getElementById("mySidepanel").style.width = "0";
+  }
+
+
+  $(document).on("click", "#btmnav a[href]", nav);
+
+  function nav(e){
+
+    e.preventDefault();
+
+    var href = $(this).attr("href");
+    $("#content").load("modules/" + href);
+
+    $("#btmnav a").fadeTo("fast", 0.7);
+    $(this).fadeTo("fast", 1); 
+
   }
