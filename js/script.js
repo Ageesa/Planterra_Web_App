158402b2a52efe5fcaceeaf6d364d982226f1b2e
Ageesa <ageesapareek321@gmail.com>
2021-04-22 22:44:07 -0400

    module content update stage - community and care

--- js/script.js
+++ js/script.js
@@ -2,6 +2,14 @@ $(window).on("load", start);
 
 function start(){
   $("#btmnav a:first").click()
+
+  $.get("#carouselExampleSlidesOnly", function (data) {
+    $("#carousel").html(data);
+    setTimeout(function(){ // just being safe
+        $('.carousel').carousel();
+    },0
+    );
+});
 }
 function openNav() {
     document.getElementById("mySidepanel").style.width = "250px";
@@ -27,9 +35,64 @@ function openNav() {
 
   }
 
-  $.get("#carouselExampleSlidesOnly", function (data) {
-               $("#carousel").html(data);
-               setTimeout(function(){ // just being safe
-                   $('.carousel').carousel();
-               },10);
-});
+  (function ($) {
+    'use strict';
+
+    /*[ File Input Config ]
+        ===========================================================*/
+    
+    try {
+    
+        var file_input_container = $('.js-input-file');
+    
+        if (file_input_container[0]) {
+    
+            file_input_container.each(function () {
+    
+                var that = $(this);
+    
+                var fileInput = that.find(".input-file");
+                var info = that.find(".input-file__info");
+    
+                fileInput.on("change", function () {
+    
+                    var fileName;
+                    fileName = $(this).val();
+    
+                    if (fileName.substring(3,11) == 'fakepath') {
+                        fileName = fileName.substring(12);
+                    }
+    
+                    if(fileName == "") {
+                        info.text("No file chosen");
+                    } else {
+                        info.text(fileName);
+                    }
+    
+                })
+    
+            });
+    
+        }
+    
+    
+    
+    }
+    catch (e) {
+        console.log(e);
+    }
+
+})(jQuery);
+ 
+
+/*-------------------------------<<<<<<<<<<<<<<<<<Community>>>>>>>>>>>>>>>>>>-------------------------------------*/
+
+var loadFile = function(event) {
+	var image = document.getElementById('output');
+	image.src = URL.createObjectURL(event.target.files[0]);
+  save();
+};
+
+ScrollReveal().reveal('.reveal1', { delay: 100 });
+ScrollReveal().reveal('.reveal2', { delay: 1000 });
+ScrollReveal().reveal('.reveal3', { delay: 1000 });
